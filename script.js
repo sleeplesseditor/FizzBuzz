09741558033c9fe07f8f61fb5af90f52d796201a
sleeplesseditor <josh@bryson.me.uk>
2017-09-13 16:16:02 +0100

    FizzBuzz
    A very simple JavaScript solution for the FizzBuzz test (with number range set 1–100).

---
+++ script.js
@@ -0,0 +1,16 @@
+//Sets range of numbers for console to print to 1–100//
+for (var i=1; i <= 20; i++)
+{
+    //Sets console to print 'Fizzbuzz' on multiple of 3 and 5//
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    //Sets console to print 'Fizz' on multiple of 3//
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    //Sets console to print 'Buzz' on multiple of 3//
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    //Sets console to print print number from range//
+    else
+        console.log(i);
+}
