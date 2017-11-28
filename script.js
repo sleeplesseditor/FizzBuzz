38c2c8477bcf84220ad4b6533ec1b6ca0d848f91
sleeplesseditor <josh@bryson.me.uk>
2017-11-28 22:06:18 +0000

    FizzBuzz

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Sets range of numbers for console to print to 1–100//
-for (var i=1; i <= 20; i++)
+for (var i=1; i <= 100; i++)
 {
     //Sets console to print 'Fizzbuzz' on multiple of 3 and 5//
     if (i % 15 == 0)
@@ -7,7 +7,7 @@ for (var i=1; i <= 20; i++)
     //Sets console to print 'Fizz' on multiple of 3//
     else if (i % 3 == 0)
         console.log("Fizz");
-    //Sets console to print 'Buzz' on multiple of 3//
+    //Sets console to print 'Buzz' on multiple of 5//
     else if (i % 5 == 0)
         console.log("Buzz");
     //Sets console to print print number from range//
