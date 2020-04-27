faa208f49f3e93a2475937c9f80159d9406fb4e6
Josh Bryson <josh@bryson.me.uk>
2020-04-27 18:41:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 //Sets range of numbers for console to print to 1–100//
-for (var i=1; i <= 100; i++)
-{
-    //Sets console to print 'Fizzbuzz' on multiple of 3 and 5//
-    if (i % 15 == 0)
-        console.log("FizzBuzz");
+for (var i=1; i <= 100; i++) {
+    // Create temp variable
+    var output = "";
     //Sets console to print 'Fizz' on multiple of 3//
-    else if (i % 3 == 0)
-        console.log("Fizz");
+    if (i % 3 == 0) {output += "Fizz";}
     //Sets console to print 'Buzz' on multiple of 5//
-    else if (i % 5 == 0)
-        console.log("Buzz");
+    if(i % 5 == 0) {output += "Buzz";}
     //Sets console to print print number from range//
-    else
-        console.log(i);
+    if(output == ""){output = i;}
+    
+    console.log(output);
 }
